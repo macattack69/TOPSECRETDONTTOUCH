2b0a7fa8067ee71c3ad1032df7116a166a924146
macattack69 <michaelryanschoeneich@gmail.com>
2023-05-15 23:26:30 -0500

    ... this is really secret please
    please do not look at this this is actually a secret plz

---
+++ yamya2w+b/scripts/Player.gd
@@ -0,0 +1,22 @@
+extends KinematicBody2D
+
+const ACCELERATION = 250
+const max_speed = 60
+const FRICTION = 400
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * max_speed, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	
+	velocity = move_and_slide(velocity)
+
