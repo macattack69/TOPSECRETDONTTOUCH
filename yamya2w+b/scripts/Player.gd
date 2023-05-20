bec4643b2214c7b47250f7348f0a0ead70ced4e1
macattack69 <michaelryanschoeneich@gmail.com>
2023-05-20 17:56:25 -0500

    hehehehe

--- yamya2w+b/scripts/Player.gd
+++ yamya2w+b/scripts/Player.gd
@@ -7,6 +7,7 @@ const FRICTION = 400
 var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -15,10 +16,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Running/blend_position", input_vector)
+		animationState.travel("Running")
 		velocity = velocity.move_toward(input_vector * max_speed, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
 	
 	velocity = move_and_slide(velocity)
