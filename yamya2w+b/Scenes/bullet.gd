40ef2e3b62ec939879cd461834593a39a756a409
macattack69 <michaelryanschoeneich@gmail.com>
2023-05-23 13:20:04 -0500

    This shit getting old ngl

---
+++ yamya2w+b/Scenes/bullet.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+export (int) var speed = 120
+
+
+
+var direction
+
+
+func _physics_process(delta: float) -> void:
+	pass
--- yamya2w+b/scripts/Player.gd
+++ yamya2w+b/scripts/Player.gd
@@ -1,4 +1,7 @@
 extends KinematicBody2D
+signal hit
+
+export (PackedScene) var bullet
 
 const ACCELERATION = 250
 const max_speed = 60
@@ -8,6 +11,7 @@ var velocity = Vector2.ZERO
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var end_of_gun = $endofgun
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -25,3 +29,12 @@ func _physics_process(delta):
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("shoot"):
+		pass
+
+func shoot():
+	var bullet_instance = bullet.instance()
+	add_child(bullet_instance)
+	bullet_instance.global_position = end_of_gun.global_position
