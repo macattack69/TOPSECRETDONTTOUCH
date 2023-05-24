53c315b8c35aa84f59e8b13cb184f443a5d00397
macattack69 <michaelryanschoeneich@gmail.com>
2023-05-24 12:45:23 -0500

    begin state machine
    started working on a state machine to handle transitioning god i hope i get this to a stable point soon

--- yamya2w+b/scripts/Player.gd
+++ yamya2w+b/scripts/Player.gd
@@ -1,24 +1,38 @@
 extends KinematicBody2D
-signal hit
+#signal hit
 
-export (PackedScene) var bullet
+#export (PackedScene) var bullet
 
 const ACCELERATION = 250
 const max_speed = 60
 const FRICTION = 400
 
+enum {
+	MOVE,
+	ATTACK,
+	DODGE,
+}
+
 var velocity = Vector2.ZERO
+
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 onready var end_of_gun = $endofgun
+var state = MOVE
+
+func _ready():
+	animationTree.active = true
 
 func _physics_process(delta):
+	move_state(delta)
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	
+
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Running/blend_position", input_vector)
@@ -30,11 +44,11 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity)
 
-func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("shoot"):
-		pass
+#func _unhandled_input(event: InputEvent) -> void:
+#	if event.is_action_pressed("shoot"):
+#		pass
 
-func shoot():
-	var bullet_instance = bullet.instance()
-	add_child(bullet_instance)
-	bullet_instance.global_position = end_of_gun.global_position
+#func shoot():
+#	var bullet_instance = bullet.instance()
+#	add_child(bullet_instance)
+#	bullet_instance.global_position = end_of_gun.global_position
